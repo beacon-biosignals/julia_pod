12e5110e0011399525588f7eef6e7f445f29e3fc
kolia <kolia.sadeghi@beacon.bio>
2021-07-29 22:21:57 +0000

    bugfix

--- add_me_to_your_PATH/sysimage.jl
+++ add_me_to_your_PATH/sysimage.jl
@@ -1,5 +1,18 @@
 using Pkg
 
+const project = Pkg.project()
+
+const modulefile = joinpath(dirname(project.path), "src", "$(project.name).jl")
+
+if project.ispackage && !isfile(modulefile)
+    @info "Project is a package, but has no src/\$JULIA_PROJECT.jl, creating an empty one."
+    mkpath(dirname(modulefile))
+    open(modulefile, "w") do io
+        println(io, "module $(project.name)")
+        println(io, "end")
+    end
+end
+
 Pkg.add("PackageCompiler")
 
 using PackageCompiler, UUIDs
