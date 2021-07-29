408f980e89705a7f363e372bafe64a01080ba5f2
kolia <kolia.sadeghi@beacon.bio>
2021-07-29 21:16:06 +0000

    works

--- add_me_to_your_PATH/sysimage.jl
+++ add_me_to_your_PATH/sysimage.jl
@@ -1,28 +1,13 @@
 using Pkg
 
-const project = Pkg.project()
-
-const modulefile = joinpath(dirname(project.path), "src", "$(project.name).jl")
-
-if project.ispackage && !isfile(modulefile)
-    @info "Project is a package, but has no src/\$JULIA_PROJECT.jl, creating an empty one."
-    mkpath(dirname(modulefile))
-    open(modulefile, "w") do io
-        println(io, "module $(project.name)")
-        println(io, "end")
-    end
-end
-
 Pkg.add("PackageCompiler")
 
 using PackageCompiler, UUIDs
 
 function main()
-    packages = [Symbol(k) for k in keys(project.dependencies)]
-    # exclude dependencies that are being developed
-    exclude = [Symbol(pi.name) for pi in values(Pkg.dependencies()) if !pi.is_tracking_registry]
+    packages = [Symbol(k) for k in keys(Pkg.project().dependencies)]
     # exclude some packages that can make sysimage creation or usage fail.
-    foreach(d -> push!(exclude, d), [:Revise, :PackageCompiler, :K8sClusterManagers])
+    exclude = [:Revise, :PackageCompiler, :K8sClusterManagers]
     println("excluding $exclude")
     packages = setdiff(packages, exclude)
     println("Creating sys image with deps being tracked from a registry:")
