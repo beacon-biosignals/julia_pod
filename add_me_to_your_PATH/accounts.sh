763fa0c8e824bd9573819bdaa0dc628d7c3a2153
Curtis Vogt <curtis.vogt@gmail.com>
2021-08-13 09:56:25 -0500

    Use Docker build secrets (#7)
    * Use Docker build secrets
    
    * Add ARG to control install of Revise and PProf
    
    * Support skipping create_sysimage
    
    * Update julia_pod and build_image
    
    * Handle no dependencies in pinned extractor
    
    * Request JSON output
    
    * Support specifying PROJECT_NAME
    
    * Fix PRIVATE_REGISTRY_URL support
    
    * Make build arguments actually optional
    
    * Use K8s secret to use GITHUB_TOKEN on pod
    
    * Update README

--- add_me_to_your_PATH/accounts.sh
+++ add_me_to_your_PATH/accounts.sh
@@ -8,10 +8,11 @@
 
 # example setup
 
-CALLER_ID=$(aws sts get-caller-identity)
+CALLER_ID=$(aws sts get-caller-identity --output json)
 AWS_ACCOUNT_ID=$(echo "$CALLER_ID" | jq -r .Account)
+AWS_USER_ID=$(echo "$CALLER_ID" | jq -r .UserId)
 REGION="us-east-2"
-PROJECT_NAME=$(echo "$CALLER_ID" | grep -oP 'assumed-role/\K[^/]*')
+PROJECT_NAME=${PROJECT_NAME:-$(echo "$CALLER_ID" | grep -oP 'assumed-role/\K[^/]*')}
 
 # above is setup for 3 required vars below
 
